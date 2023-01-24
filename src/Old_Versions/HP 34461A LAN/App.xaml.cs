f1e1cf4748d5383278806c4671d13c8ab03e37fa
Miguel <miguel.leon@jlm-innovation.de>
2023-01-24 17:05:32 +0100

    Added HP 34461A LAN Project as a copy of HP 34401A (VISA GPIB) Migrated packages.config to PackageReference Added support to 34461A Tested over GPIB, next steps: add support over TCP/IP

---
+++ src/Old_Versions/HP 34461A LAN/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace HP_34461A
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
